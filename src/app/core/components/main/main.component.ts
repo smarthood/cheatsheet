addcccec9fca362d1f18ba07c60b4ff75d4a9934
smarthood <renolddickson18@gmail.com>
2023-04-21 15:32:07 +0530

    dialog

--- src/app/core/components/main/main.component.ts
+++ src/app/core/components/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, TemplateRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-main',
@@ -8,11 +9,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class MainComponent {
   collection!: FormGroup;
+  @ViewChild('collectiondialog') collectiondialog!: TemplateRef<any>;
   @HostListener('window:keydown', ['$event']) onKeyDown(e: any) {
     if (e.shiftKey && e.keyCode == 76) {
-      sessionStorage.setItem('login', 'truewai');
+      this.dialog.open(this.collectiondialog);
     }
   }
+  constructor(private dialog: MatDialog) {}
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
