e5b42810f34b3331d0f3e8ff537a30fab332bf07
smarthood <renolddickson18@gmail.com>
2023-05-26 13:01:15 +0530

    tr

--- src/app/core/services/Guard/login.guard.spec.ts
+++ src/app/core/services/Guard/login.guard.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { LoginGuard } from './login.guard';
 
-describe('LoginGuard', () => {
+fdescribe('LoginGuard', () => {
   let guard: LoginGuard;
 
   beforeEach(() => {
@@ -12,5 +12,17 @@ describe('LoginGuard', () => {
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+  it('should call canActivate', () => {
+    sessionStorage.setItem('currentUserToken', '{}');
+    const guardTest = guard.canActivate(null!, null!);
+    expect(guardTest).toBeTrue();
+    expect(guard.canActivate).toBeDefined();
+  });
+  it('should call canActivate else', () => {
+    sessionStorage.removeItem('currentUserToken');
+    const guardTest = guard.canActivate(null!, null!);
+    expect(guardTest).toBeFalse();
+    expect(guard.canActivate).toBeDefined();
   });
 });
